5431396228627a93668bc8f87bb7694901c133ad
Rougeot <r_rougeot@hotmail.fr>
2020-03-22 15:11:12 +0100

    Default file restored

--- Executable.m
+++ Executable.m
@@ -3,16 +3,15 @@
 
 addpath('./Fourier_Optics_Tools');
 
+%%%%%
 clear all
 
 % Generate set of parameters
 Set_Parameters;
 
-%%%%%
-
 Study_name = '';
-Filename_parameters = ['/home/*/' Study_name, '_param.mat'];
-Filename_results = ['/home/*/',Study_name, '_results.mat'];
+Filename_parameters = ['' Study_name, '_param.mat'];
+Filename_results = ['',Study_name, '_results.mat'];
 
 % Save current parameters of the study
 save(Filename_parameters);
